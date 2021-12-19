e36181ce4e5d823aa5c7960569f2827f474a4cdd
yoav-es <70217988+yoav-es@users.noreply.github.com>
2021-12-19 17:52:06 +0200

    Update secrets.h

--- secrets.h
+++ secrets.h
@@ -7,8 +7,8 @@
 
 
 //smtp server
-#define SENDER_ACCOUNT "esp32tlc@gmail.com"
-#define SENDER_PASS    "QCqrvz9))"
+#define SENDER_ACCOUNT ""
+#define SENDER_PASS    ""
 #define SMTP_SRV     "smtp.gmail.com"
 #define SMTP_PORT    465
 #define EMAIL_SUB    "[ALERT] ESP32 log"
